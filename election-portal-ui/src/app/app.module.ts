44df6aaab7ba98e1af4ebcc0eafbd40462aefab8
ayushmsihra1803 <ayushmishra0810@gmail.com>
2022-06-20 20:41:23 +0530

    routing added for the user login

--- election-portal-ui/src/app/app.module.ts
+++ election-portal-ui/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserLoginComponent } from './pages/auth/user-login/user-login.component';
 import { UserSignupComponent } from './pages/auth/user-signup/user-signup.component';
 import { PartyAdminSignupComponent } from './pages/auth/party-admin-signup/party-admin-signup.component';
 import { PartyAdminLoginComponent } from './pages/auth/party-admin-login/party-admin-login.component';
@@ -13,7 +12,6 @@ import { AdminSignUpComponent } from './pages/auth/admin-sign-up/admin-sign-up.c
 @NgModule({
   declarations: [
     AppComponent,
-    UserLoginComponent,
     UserSignupComponent,
     PartyAdminSignupComponent,
     PartyAdminLoginComponent,
---
+++ election-portal-ui/src/app/pages/auth/user-login/user-login.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { UserLoginRoutingModule } from './user-login-routing.module';
+import { UserLoginComponent } from './user-login.component';
+
+@NgModule({
+  declarations: [UserLoginComponent],
+  imports: [CommonModule, UserLoginRoutingModule],
+  providers: [],
+})
+export class UserloginModule {}
